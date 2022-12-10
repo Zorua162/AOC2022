552cf44bcc8df32d28ea898bdc429fa38c757979
zorua162 <jakeirwin03@gmail.com>
2022-12-10 22:51:56 +0000

    Pivoting to method in day 7 using a file struct system instead

--- day07/Part1/solution.go
+++ day07/Part1/solution.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"os"
+	"sort"
+
+	// "sort"
 	"strconv"
 	"strings"
 	// "math"
@@ -14,21 +17,51 @@ func check(e error) {
 	}
 }
 
-func search_file_tree(start string, files map[string][]string, sizes map[string]int, total_sizes map[string]int) map[string]int {
+type File struct {
+	filename string
+	folder bool
+	contains_files []string
+	size int
+}
 
-	return total_sizes
+func Contains(list []string, x string) bool {
+	for _, item := range list {
+		if item == x {
+			return true
+		}
+	}
+	return false
+}
+
+func search_file_tree(start string, files map[string][]string, sizes map[string]int, total_sizes map[string]int, already_searched []string) (map[string]int, []string) {
+	total_sizes[start] = sizes[start]
+	// Deal with possible infinite loops that somehow exist
+	if Contains(already_searched, start) {
+		return total_sizes, already_searched
+	}
+	already_searched = append(already_searched, start)
+	for _, elem := range files[start] {
+		fmt.Println(elem)
+		_, exists := files[elem]
+		if exists {
+			total_sizes, already_searched = search_file_tree(elem, files, sizes, total_sizes, already_searched)
+			total_sizes[start] += sizes[elem]
+		}
+	}
+
+	return total_sizes, already_searched
 }
 
 func main() {
-	// dat, err := os.ReadFile("./../dat")
-	dat, err := os.ReadFile("./../example_dat")
+	dat, err := os.ReadFile("./../dat")
+	// dat, err := os.ReadFile("./../example_dat")
 	check(err)
 	// fmt.Println(string(dat))
 
 	// s := strings.Fields(string(dat[:]))
 	s := strings.Split(string(dat), "\n")
 	// fmt.Println(s)
-	var count int = 0
+	// var count int = 0
 	// var last int = math.MaxInt64
 	// the key is the folder name and the value is a list of sub folders or files
 	files := make(map[string][]string, 1)
@@ -56,12 +89,40 @@ func main() {
 		// }
 		// last = intVal
 		// fmt.Println(index, element, components)
-		fmt.Println(files, sizes)
-		fmt.Println(count)
-		total_sizes := make(map[string]int)
-		total_sizes = search_file_tree("/", files, sizes, total_sizes)
+		// fmt.Println(files, sizes)
+		// fmt.Println(count)
+	}
+	fmt.Println(sizes, files)
+	total_sizes := make(map[string]int)
+	var already_searched []string
+	fmt.Println("Starting to search tree")
+	total_sizes, already_searched = search_file_tree("/", files, sizes, total_sizes, already_searched)
+	fmt.Println(total_sizes, already_searched)
+
+	keys := make([]string, 0, len(total_sizes))
+
+	for key := range total_sizes {
+		keys = append(keys, key)
 	}
 
-	// Recursive function to dive into the map and add the values as it comes back out
+	sort.SliceStable(keys, func(i, j int) bool {
+		return total_sizes[keys[i]] < total_sizes[keys[j]]
+	})
+
+	fmt.Println(keys)
+
+	count := 0
+
+	for _, key := range keys {
+		fmt.Println(key)
+		value := total_sizes[key]
+		if value > 100000 {
+			continue
+		}
+		count += value
+	}
+	fmt.Println(count)
+
+	// 748557
 
 }
